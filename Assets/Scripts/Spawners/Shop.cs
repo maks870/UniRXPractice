d757d5a4ab6e8cdbba1f9ad967f5df9081478a1c
maks870 <maks7-2014@inbox.ru>
2023-06-06 13:32:18 +0300

    End shopLogic

--- Assets/Scripts/Spawners/Shop.cs
+++ Assets/Scripts/Spawners/Shop.cs
@@ -1,14 +1,22 @@
-using System.Collections;
+using TMPro;
 using UniRx;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    private CompositeDisposable disposable = new CompositeDisposable();
+    [Header("Prices")]
+    [SerializeField] private int increaseCost = 10;
+    [SerializeField] private int increaseRise = 10;
+    [SerializeField] private int upgradeCost = 10;
+    [SerializeField] private int upgradeRise = 10;
 
+    [Space(5)]
+    [Header("Dependencies")]
     [SerializeField] private CoinSpawner spawner;
     [SerializeField] private Player player;
     [SerializeField] private UIShop shopUI;
+
+    private CompositeDisposable disposable = new CompositeDisposable();
 
     private void OnDisable()
     {
@@ -17,12 +25,50 @@ public class Shop : MonoBehaviour
 
     private void Start()
     {
-        shopUI.tickSpawnerIncreaseCount.OnClickAsObservable().Subscribe((value) => spawner.TickSpawner.IncreaseCount()).AddTo(disposable);
-        spawner.TickSpawner.increaseCommand.Subscribe((value) => { shopUI.tickSpawnCount.text = value.ToString(); }).AddTo(disposable);
+        shopUI.tickSpawnerIncreaseCount.OnClickAsObservable().Subscribe((value) => BuyTickIncrease()).AddTo(disposable);
+        spawner.TickSpawner.increaseCommand.Subscribe((value) => { shopUI.tickSpawnCountTxt.text = value.ToString(); }).AddTo(disposable);
+        shopUI.tickSpawnerIncreaseCount.GetComponentInChildren<TextMeshProUGUI>().text = increaseCost.ToString();
 
-        shopUI.tickSpawnerUpgrade.OnClickAsObservable().Subscribe((value) => spawner.TickSpawner.Upgrade()).AddTo(disposable);
+
+        shopUI.tickSpawnerUpgrade.OnClickAsObservable().Subscribe((value) => BuyTickUpgrade()).AddTo(disposable);
         TikSpawner tikSpawner = (TikSpawner)spawner.TickSpawner;
-        tikSpawner.upgradeCommand.Subscribe((value) => { shopUI.tickTime.text = value.ToString(); }).AddTo(disposable);
+        tikSpawner.upgradeCommand.Subscribe((value) => { shopUI.tickTimeTxt.text = value.ToString(); }).AddTo(disposable);
+        shopUI.tickSpawnerUpgrade.GetComponentInChildren<TextMeshProUGUI>().text = upgradeCost.ToString();
     }
 
+    private bool SpendCoin(int spendCount)
+    {
+        int coin = player.coinCount.Value;
+
+        if (coin - spendCount <= 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(shopUI.NotifyLackCoin());
+
+            return false;
+        }
+
+        player.coinCount.Value -= spendCount;
+        return true;
+    }
+
+    private void BuyTickIncrease()
+    {
+        if (!SpendCoin(increaseCost))
+            return;
+
+        spawner.TickSpawner.IncreaseCount();
+        increaseCost += increaseRise;
+        shopUI.tickSpawnerIncreaseCount.GetComponentInChildren<TextMeshProUGUI>().text = increaseCost.ToString();
+    }
+
+    private void BuyTickUpgrade()
+    {
+        if (!SpendCoin(upgradeCost))
+            return;
+
+        spawner.TickSpawner.Upgrade();
+        upgradeCost += upgradeRise;
+        shopUI.tickSpawnerUpgrade.GetComponentInChildren<TextMeshProUGUI>().text = upgradeCost.ToString();
+    }
 }
--- Assets/Scripts/Spawners/UIShop.cs
+++ Assets/Scripts/Spawners/UIShop.cs
@@ -1,22 +1,49 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIShop : MonoBehaviour
 {
-    public TextMeshProUGUI tickSpawnCount;
-    public TextMeshProUGUI tickTime;
+    public TextMeshProUGUI tickSpawnCountTxt;
+    public TextMeshProUGUI tickTimeTxt;
+
+    public TextMeshProUGUI lackCoinTxt;
+
     public Button tickSpawnerUpgrade;
     public Button tickSpawnerIncreaseCount;
 
     void Start()
     {
-
+        if (lackCoinTxt.gameObject.activeInHierarchy)
+            lackCoinTxt.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator NotifyLackCoin()
     {
+        float blurTime = 0.5f;
+        float showTime = 0.5f;
+        Color textColor = lackCoinTxt.color;
 
+        textColor.a = 0;
+        lackCoinTxt.gameObject.SetActive(true);
+
+        for (float t = 0; t < blurTime; t += Time.deltaTime)
+        {
+            textColor.a = Mathf.InverseLerp(0, blurTime, t);
+            lackCoinTxt.color = textColor;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        yield return new WaitForSeconds(showTime);
+
+        for (float t = blurTime; t > 0; t -= Time.deltaTime)
+        {
+            textColor.a = Mathf.InverseLerp(0, blurTime, t);
+            lackCoinTxt.color = textColor;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        lackCoinTxt.gameObject.SetActive(false);
     }
 }
